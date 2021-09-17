00ea91aa848760853cfa1fd05dc2c687a1297d78
Infarh <shmachilin@yandex.ru>
2021-09-17 20:37:51 +0300

    Использование проекций DTO-моделей

--- Services/WebStore.WebAPI/Controllers/ProductsApiController.cs
+++ Services/WebStore.WebAPI/Controllers/ProductsApiController.cs
@@ -17,7 +17,7 @@ namespace WebStore.WebAPI.Controllers
         public IActionResult GetSections()
         {
             var sections = _ProductData.GetSections();
-            return Ok(sections);
+            return Ok(sections.ToDTO());
         }
 
         [HttpGet("sections/{id:int}")]
@@ -27,22 +27,14 @@ namespace WebStore.WebAPI.Controllers
             if (section is null)
                 return NotFound(id);
 
-            var section_dto = new SectionDTO
-            {
-                Id = section.Id,
-                Name = section.Name,
-                Order = section.Order,
-                ParentId = section.ParentId
-            };
-
-            return Ok(section_dto);
+            return Ok(section.ToDTO());
         }
 
         [HttpGet("brands")]
         public IActionResult GetBrands()
         {
             var brands = _ProductData.GetBrands();
-            return Ok(brands);
+            return Ok(brands.ToDTO());
         }
 
         [HttpGet("brands/{id:int}")]
@@ -51,14 +43,14 @@ namespace WebStore.WebAPI.Controllers
             var brand = _ProductData.GetBrand(id);
             if (brand is null)
                 return NotFound(id);
-            return Ok(brand);
+            return Ok(brand.ToDTO());
         }
 
         [HttpPost]
         public IActionResult GetProducts(ProductFilter Filter = null)
         {
             var products = _ProductData.GetProducts(Filter ?? new());
-            return Ok(products);
+            return Ok(products.ToDTO());
         }
 
         [HttpGet("{id:int}")]
@@ -67,7 +59,7 @@ namespace WebStore.WebAPI.Controllers
             var product = _ProductData.GetProductById(id);
             if (product is null)
                 return NotFound(id);
-            return Ok(product);
+            return Ok(product.ToDTO());
         }
     }
 }
