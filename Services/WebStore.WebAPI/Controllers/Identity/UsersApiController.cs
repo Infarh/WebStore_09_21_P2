4212e5fa7fe771c7f87d7668904da2a5fe9fa8d0
Infarh <shmachilin@yandex.ru>
2021-09-21 19:34:57 +0300

    Заготовки контроллеров WebAPI для системы Identity

--- Services/WebStore.WebAPI/Controllers/Identity/UsersApiController.cs
+++ Services/WebStore.WebAPI/Controllers/Identity/UsersApiController.cs
@@ -1,4 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebStore.DAL.Context;
+using WebStore.Domain.Entities.Identity;
 using WebStore.Interfaces;
 
 namespace WebStore.WebAPI.Controllers.Identity
@@ -7,5 +13,15 @@ namespace WebStore.WebAPI.Controllers.Identity
     [Route(WebAPIAddresses.Identity.Users)]
     public class UsersApiController : ControllerBase
     {
+        public readonly UserStore<User, Role, WebStoreDB> _UserStore;
+
+        public UsersApiController(WebStoreDB db)
+        {
+            _UserStore = new UserStore<User, Role, WebStoreDB>(db);
+            //_UserStore.AutoSaveChanges = false; // выключение отслеживания изменений для повышения быстродействия
+        }
+
+        [HttpGet("all")]
+        public async Task<IEnumerable<User>> GetAllUsers() => await _UserStore.Users.ToArrayAsync();
     }
 }
