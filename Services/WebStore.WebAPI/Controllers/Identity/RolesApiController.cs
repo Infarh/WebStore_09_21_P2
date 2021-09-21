f062161fdf2e2a10e0bfd1e39f796bbf5e5db60e
Infarh <shmachilin@yandex.ru>
2021-09-21 19:27:51 +0300

    Рефакторинг системы адресов WebAPI через единый класс с константами

---
+++ Services/WebStore.WebAPI/Controllers/Identity/RolesApiController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Interfaces;
+
+namespace WebStore.WebAPI.Controllers.Identity
+{
+    [ApiController]
+    [Route(WebAPIAddresses.Identity.Roles)]
+    public class RolesApiController : ControllerBase
+    {
+    }
+}
---
+++ Services/WebStore.WebAPI/Controllers/Identity/UsersApiController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Interfaces;
+
+namespace WebStore.WebAPI.Controllers.Identity
+{
+    [ApiController]
+    [Route(WebAPIAddresses.Identity.Users)]
+    public class UsersApiController : ControllerBase
+    {
+    }
+}
