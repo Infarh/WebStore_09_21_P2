efc016d295156f2a7f59f04f7f8b9922abe70cd8
Infarh <shmachilin@yandex.ru>
2021-10-01 19:46:55 +0300

    Доработка логики таг-хелпера активации ссылки

--- UI/WebStore/TagHelpers/ActiveRoute.cs
+++ UI/WebStore/TagHelpers/ActiveRoute.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.AspNetCore.Razor.TagHelpers;
@@ -10,7 +11,9 @@ namespace WebStore.TagHelpers
     [HtmlTargetElement(Attributes = AttributeName)]
     public class ActiveRoute : TagHelper
     {
-        public const string AttributeName = "ws-is-active-route";
+        private const string AttributeName = "ws-is-active-route";
+
+        private const string IgnoreAction = "ws-ignore-action";
 
         [HtmlAttributeName("asp-controller")]
         public string Controller { get; set; }
@@ -29,13 +32,15 @@ namespace WebStore.TagHelpers
 
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
-            if (IsActive())
+            var is_ignore_action = output.Attributes.RemoveAll(IgnoreAction);
+
+            if (IsActive(is_ignore_action))
                 MakeActive(output);
 
             output.Attributes.RemoveAll(AttributeName);
         }
 
-        private bool IsActive()
+        private bool IsActive(bool IsIgnoreAction)
         {
             var route_values = ViewContext.RouteData.Values;
 
@@ -44,7 +49,7 @@ namespace WebStore.TagHelpers
 
             //if (!string.IsNullOrEmpty(Action) && !string.Equals(Action, current_action))
             //    return false;
-            if(Action is { Length: > 0 } action && !string.Equals(action, route_action))
+            if (!IsIgnoreAction && Action is { Length: > 0 } action && !string.Equals(action, route_action))
                 return false;
 
             if (Controller is { Length: > 0 } controller && !string.Equals(controller, route_controller))
@@ -61,11 +66,11 @@ namespace WebStore.TagHelpers
         {
             var class_attribute = output.Attributes.FirstOrDefault(attr => attr.Name == "class");
 
-            if(class_attribute is null)
+            if (class_attribute is null)
                 output.Attributes.Add("class", "active");
             else
             {
-                if(class_attribute.Value.ToString()?.Contains("active") ?? false)
+                if (class_attribute.Value.ToString()?.Contains("active") ?? false)
                     return;
 
                 output.Attributes.SetAttribute("class", class_attribute.Value + " active");
