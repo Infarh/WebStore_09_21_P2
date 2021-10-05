188e8bd811ad445a0e8a33e1a72fdf7e4ea1db2d
Infarh <shmachilin@yandex.ru>
2021-10-05 19:18:15 +0300

    Логика работы визуального компонента корзины

--- UI/WebStore/Components/CartViewComponent.cs
+++ UI/WebStore/Components/CartViewComponent.cs
@@ -1,9 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Interfaces.Services;
 
 namespace WebStore.Components
 {
     public class CartViewComponent : ViewComponent
     {
-        public IViewComponentResult Invoke() => View();
+        private readonly ICartService _CartService;
+
+        public CartViewComponent(ICartService CartService) => _CartService = CartService;
+
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.Count = _CartService.GetViewModel().ItemsCount;
+            return View();
+        }
     }
 }
